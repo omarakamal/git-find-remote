d71715bfbc6417b836f8cd3bd43d3b236141df35
omarakamal <omaraakamal@gmail.com>
2026-07-15 16:40:54 +0300

    feat: add error feedback messages

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 const search = require("../lib/search")
+const colors = require('colors')
 
 const remoteUrl = process.argv[2]
 
 if (!remoteUrl) {
-    console.log("Please provide a git remote URL.")
+    console.log(colors.red("Please provide a git remote URL."))
     process.exit(1)
 }
 
